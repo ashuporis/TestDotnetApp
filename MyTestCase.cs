86a2ddd55f3a55b53d432b3c26d41dc088ab2d64
ashuporis <38080739+ashuporis@users.noreply.github.com>
2018-04-04 13:24:09 -0400

    Add files via upload

---
+++ MyTestCase.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestDemo
+{
+    [TestFixture]
+    class MyTestCase
+    {
+        [TestCase]
+        public void Add()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(30, math.Add(20, 10));
+        }
+
+        [TestCase]
+        public void Sub()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(10, math.Subtract(20, 10));
+        }
+    }
+}
